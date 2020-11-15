49642a58f6a3401f0605f19f15049234aec08ddc
Guillaume Vanleene <guillaume.vanleene2005@gmail.com>
2020-11-15 15:04:13 +0100

    v2.2
    Implémentation de 'demande_coordonnees' pour éviter la redondance pour demander des coordonnées

--- math/geometrie/coordonnées/milieu_entre_2_points.py
+++ math/geometrie/coordonnées/milieu_entre_2_points.py
@@ -2,56 +2,37 @@
 # Script permettant de connaître la distance entre 2 points donnés par coordonnés
 # v 1.1
 
-def milieu_entre_2_points(A_coordonnées, B_coordonnées):
-    return [round((A_coordonnées[0] + B_coordonnées[0]) / 2, 3), round((A_coordonnées[1] + B_coordonnées[1]) / 2, 3)]
+def milieu_entre_2_points(a_coordonnées, b_coordonnées):
+    return [round((a_coordonnées[0][0] + b_coordonnées[0][0]) / 2, 3), round((a_coordonnées[0][1] + b_coordonnées[0][1]) / 2, 3)]
+
+def demander_coordonnees(coordonnee_nom):
+    print("Coordonnées du point {0} (x y): ".format(coordonnee_nom))
+
+    # ( [x, y], nom )
+    coordonnee = ([None] * 2, coordonnee_nom)
+
+    while(True):
+        user_input = input(" > ").split(" ")
+        
+        try:
+            coordonnee[0][0] = int(user_input[0])
+        except (ValueError, IndexError):
+            print("Abscisse incorrect")
+            continue
+        try:
+            coordonnee[0][1] = int(user_input[1])
+        except (ValueError, IndexError):
+            print("Ordonnée incorrecte")
+            continue
+        break
+    return coordonnee
 
 def distance_entre_2_points_calc():
     while(True):
-        A_coordonnées = [None] * 3
-        B_coordonnées = [None] * 3
 
-        print("Coordonnées du point A (x y): ")
-
-        while(True):
-            A_coordonnées[0] = input(" > ").split(" ")
-                
-            try:
-                A_coordonnées[1] = int(A_coordonnées[0][0])
-            except (ValueError, IndexError):
-                print("Abscisse incorrect")
-                continue
-            try:
-                A_coordonnées[2] = int(A_coordonnées[0][1])
-            except (ValueError, IndexError):
-                print("Ordonnée incorrecte")
-                continue
-            break
-
-        print("Coordonnées du point B (x y): ")
+        milieu = milieu_entre_2_points(demander_coordonnees("A"), demander_coordonnees("B"))
         
-        while(True):
-            B_coordonnées[0] = input(" > ").split(" ")
-            
-            try:
-                B_coordonnées[1] = int(B_coordonnées[0][0])
-            except (ValueError, IndexError):
-                print("Abscisse incorrect")
-                continue
-            try:
-                B_coordonnées[2] = int(B_coordonnées[0][1])
-            except (ValueError, IndexError):
-                print("Ordonnée incorrecte")
-                continue
-            break
-
-        milieu = milieu_entre_2_points([A_coordonnées[1], A_coordonnées[2]], [B_coordonnées[1], B_coordonnées[2]])
-
-        if milieu[0].is_integer():
-            milieu[0] = int(milieu[0])
-        if milieu[1].is_integer():
-            milieu[1] = int(milieu[1])
-        
-        print("\nLe mileu est de : ({0} ; {1})\n".format(milieu[0], milieu[1]))
+        print("\nLe mileu est de : ({0} ; {1})\n".format(milieu[0], milieu[0]))
 
 if __name__ == "__main__":
     distance_entre_2_points_calc()
